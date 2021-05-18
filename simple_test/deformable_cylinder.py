1dcf0579a14519319de2de459ac48800d883eca4
laurent LEQUIEVRE <laurent.lequievre@uca.fr>
2021-05-18 16:06:40 +0200

    Aapted physics parameters

--- simple_test/deformable_cylinder.py
+++ simple_test/deformable_cylinder.py
@@ -68,7 +68,11 @@ base_position_cylinder = [1.775,0.0,0.52]
 
 #cylinderId = p.loadSoftBody("deformable_object/tetra_cylinder_50_cm.vtk", basePosition = base_position_cylinder, baseOrientation=base_orientation_cylinder, scale = 1.0, mass = 100, collisionMargin = 0.005, useMassSpring=0, useBendingSprings=0, useNeoHookean = 1, NeoHookeanMu = 83200, NeoHookeanLambda = 83200, NeoHookeanDamping = 1000, springElasticStiffness=0.0, springDampingStiffness=0.0, springBendingStiffness=0.0, springDampingAllDirections=0, frictionCoeff=.5, useFaceContact=0, useSelfCollision=0, repulsionStiffness=0.0)
 
-cylinderId = p.loadSoftBody("deformable_object/tetra_cylinder_50_cm.vtk", basePosition = base_position_cylinder, baseOrientation=base_orientation_cylinder, scale = 1.0, mass = 10.0, collisionMargin = 0.01, useNeoHookean = 1, NeoHookeanMu = 83200, NeoHookeanLambda = 83200, NeoHookeanDamping = 1000, frictionCoeff=.5, repulsionStiffness=800.0)
+#cylinderId = p.loadSoftBody("deformable_object/tetra_cylinder_50_cm.vtk", basePosition = base_position_cylinder, baseOrientation=base_orientation_cylinder, scale = 1.0, mass = 5.0, collisionMargin = 0.01, useNeoHookean = 1, NeoHookeanMu = 83200, NeoHookeanLambda = 83200, NeoHookeanDamping = 1000, frictionCoeff=.5)
+
+
+cylinderId = p.loadSoftBody("deformable_object/tetra_cylinder_50_cm.vtk", basePosition = base_position_cylinder, baseOrientation=base_orientation_cylinder, scale = 1.0, mass = 5.0, collisionMargin = 0.01, useNeoHookean = 1, NeoHookeanMu = 6000, NeoHookeanLambda = 6000, NeoHookeanDamping = 20, frictionCoeff=5)
+
 
 if (debug_print_vertices):
     print_mesh_data_vertices(cylinderId)
@@ -158,7 +162,7 @@ while True:
                                     physicsClientId=physics_client_id)
                                     
     p.setJointMotorControl2(bodyUniqueId=cube_prismatic_left_Id, jointIndex=3, controlMode=p.POSITION_CONTROL, targetPosition=slider_value_gripper_left,
-                                    physicsClientId=physics_client_id)
+                                   physicsClientId=physics_client_id)
                                     
                                     
                                     
